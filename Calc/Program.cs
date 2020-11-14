907348f670572ef2b752623e66152d183396c968
1VARIATUS1 <inetultra@gmail.com>
2020-11-14 13:05:11 +0200

    Добавьте файлы проекта.

---
+++ Calc/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                double number1;
+                bool parseResult;
+                string input;
+
+
+                do
+                {
+                    Console.Write("Enter number 1 please: ");
+                    input = Console.ReadLine();
+                    parseResult = double.TryParse(input, out number1);
+                }
+                while (!parseResult);
+
+                double number2;
+
+                Console.Write("Enter number 2 please: ");
+                while (!double.TryParse(Console.ReadLine(), out number2))
+                {
+                    Console.Write("Enter number 2 please: ");
+                }
+
+
+                bool correctOperator;
+                do
+                {
+                    Console.Write("Enter your operation:'+','-','/','*','^','√'");
+                    string userInput = Console.ReadLine();
+                    correctOperator = true;
+
+                    switch (userInput)
+                    {
+                        case "+":
+                            Console.WriteLine(number1 + number2);
+                            break;
+                        case "-":
+                            Console.WriteLine(number1 - number2);
+                            break;
+                        case "/":
+                            if (number2 == 0)
+                            {
+                                Console.WriteLine("Invalid operation - (number 2) == 0");
+                                correctOperator = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine(number1 / number2);
+                            }
+                            break;
+                        case "*":
+                            Console.WriteLine(number1 * number2);
+                            break;
+                        case "^":
+                            Console.WriteLine(Math.Pow(number1, number2));
+                            break;
+                        case "√":
+                            Console.WriteLine($"{Math.Sqrt(number1)} and {Math.Sqrt(number2)}");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid operation!");
+                            correctOperator = false;
+                            break;
+                    }
+                }
+                while (!correctOperator);
+
+                
+                Console.WriteLine("Do you want to continue? Y/N");
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Y)
+                    repeat = true;
+                else
+                    repeat = false;
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Homework_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Введите пятизначное число:");
+            int.TryParse(Console.ReadLine(), out num);
+            if (num > 9999 && num < 100000)
+            {
+                Console.WriteLine($"1 цифра равна{num / 10000}");
+                Console.WriteLine($"2 цифра равна{(num / 1000) % 10}");
+                Console.WriteLine($"3 цифра равна{(num / 100) % 10}");
+                Console.WriteLine($"4 цифра равна{(num / 10) % 10}");
+                Console.WriteLine($"5 цифра равна{num % 10}");
+            }
+            else
+            {
+                if (num <= 9999)
+                    Console.WriteLine("Ваше число меньше пятизначного!");
+                if (num >= 100000)
+                    Console.WriteLine("Ваше число больше пятизначного!");
+            }
+
+            int max = num % 10; 
+            num = num / 10;
+            do
+            {
+               if (num % 10 > max)
+                   max = num % 10;
+               num = num / 10;               
+            }
+            while (num > 0);
+
+            Console.WriteLine($"Максимальная цифра:{max}");
+        }
+    }
+}
